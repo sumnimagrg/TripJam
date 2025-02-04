77600e9c119db5615a546520f96a05451028156d
monitonyooo <monikamagar017@gmail.com>
2025-02-04 14:42:19 +0545

    try git

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @include('admin.inc.main')
 @section('container')
-
+<h1>Hello try</h1>
             <div class="container-xxl flex-grow-1 container-p-y">
               <div class="row">
                 <div class="col-lg-8 mb-4 order-0">
